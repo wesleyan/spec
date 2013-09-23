184e8a3393096e68ebfbe30c38a840a65d5fa13e
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-09-23 16:19:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//New NED
+
+var express = require('express');
+
+var app = express();
+var path = require('path');
+
+var date = new Date();
+var diff = date.getTimezoneOffset()/60;
+
+var d = date.getDate();
+var m = date.getMonth();
+var y = date.getFullYear();
+
+var events = [
+				{
+					id: 999,
+					title: 'Luncheon: Division III NSM Luncheon',
+					start: new Date(y, m, d, 9-diff, 45),
+					end: new Date(y, m, d, 13-diff, 30),
+					backgroundColor: '#5bb75b'
+				},
+				{
+					id: 999,
+					title: 'Reception: Prof. F. Reeve Memorial & Reception',
+					start: new Date(y, m, d, 13-diff, 0),
+					end: new Date(y, m, d, 18-diff, 0),
+					backgroundColor: '#5bb75b'
+				},
+				{
+					id: 999,
+					title: 'Dance Presentation',
+					start: new Date(y, m, d, 13-diff, 15),
+					end: new Date(y, m, d, 14-diff, 30),
+					backgroundColor: '#5bb75b'
+				},
+				{
+					title: 'Meeting',
+					start: new Date(y, m, d+1, 10-diff, 30),
+					backgroundColor:'#da4f49'
+				},
+				{
+					title: 'Lunch',
+					start: new Date(y, m, d, 12, 0),
+					end: new Date(y, m, d, 14, 0),
+					backgroundColor:'#faa732'
+				},
+				{
+					title: 'Birthday Party',
+					start: new Date(y, m, d+1, 19-diff, 0),
+					end: new Date(y, m, d+1, 22-diff, 30),
+					backgroundColor: '#5bb75b'
+					//url: 'http://google.com/'
+				}
+			];
+
+app.get("/events", function(req,res) {
+    console.log("New request for " + req.url);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(events).toString("utf-8"));
+    res.end();
+});
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+app.listen(8080);
+console.log('Listening on port 8080');
