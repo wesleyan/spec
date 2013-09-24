6e0dcee643e1c60ed28373ca53cfed1db2c69d9d
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-09-24 15:53:13 -0400

    colors updated, texts now legible

--- app.js
+++ app.js
@@ -18,38 +18,38 @@ var events = [
 					title: 'Luncheon: Division III NSM Luncheon',
 					start: new Date(y, m, d, 9-diff, 45),
 					end: new Date(y, m, d, 13-diff, 30),
-					backgroundColor: '#5bb75b'
+					backgroundColor: '#097054'
 				},
 				{
 					id: 999,
 					title: 'Reception: Prof. F. Reeve Memorial & Reception',
 					start: new Date(y, m, d, 13-diff, 0),
 					end: new Date(y, m, d, 18-diff, 0),
-					backgroundColor: '#5bb75b'
+					backgroundColor: '#097054'
 				},
 				{
 					id: 999,
 					title: 'Dance Presentation',
 					start: new Date(y, m, d, 13-diff, 15),
 					end: new Date(y, m, d, 14-diff, 30),
-					backgroundColor: '#5bb75b'
+					backgroundColor: '#097054'
 				},
 				{
 					title: 'Meeting',
 					start: new Date(y, m, d+1, 10-diff, 30),
-					backgroundColor:'#da4f49'
+					backgroundColor:'#9E3B33'
 				},
 				{
 					title: 'Lunch',
 					start: new Date(y, m, d, 12, 0),
 					end: new Date(y, m, d, 14, 0),
-					backgroundColor:'#faa732'
+					backgroundColor:'#E48743'
 				},
 				{
 					title: 'Birthday Party',
 					start: new Date(y, m, d+1, 19-diff, 0),
 					end: new Date(y, m, d+1, 22-diff, 30),
-					backgroundColor: '#5bb75b'
+					backgroundColor: '#666666'
 					//url: 'http://google.com/'
 				}
 			];
