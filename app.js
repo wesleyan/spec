0151abd12d5aced84e1a7bc888e67cd1729969b5
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-09-26 12:50:26 -0400

    feature: event object additional features & color variable

--- app.js
+++ app.js
@@ -12,40 +12,50 @@ var d = date.getDate();
 var m = date.getMonth();
 var y = date.getFullYear();
 
+var color = {
+	'green':'#097054',
+	'red':'#9E3B33'
+}
+
 var events = [
 				{
-					id: 999,
+					id: 1,
 					title: 'Luncheon: Division III NSM Luncheon',
+					staffAdded: 1,
+					staffNeeded: 1,
 					start: new Date(y, m, d, 9-diff, 45),
 					end: new Date(y, m, d, 13-diff, 30),
-					backgroundColor: '#097054'
+					backgroundColor: color.green
 				},
 				{
-					id: 999,
+					id: 2,
 					title: 'Reception: Prof. F. Reeve Memorial & Reception',
 					start: new Date(y, m, d, 13-diff, 0),
 					end: new Date(y, m, d, 18-diff, 0),
 					backgroundColor: '#097054'
 				},
 				{
-					id: 999,
+					id: 3,
 					title: '<i class="icon-facetime-video icon-white"></i> Dance Presentation',
 					start: new Date(y, m, d, 13-diff, 15),
 					end: new Date(y, m, d, 14-diff, 30),
 					backgroundColor: '#097054'
 				},
 				{
+					id: 4,
 					title: 'Meeting',
 					start: new Date(y, m, d+1, 10-diff, 30),
-					backgroundColor:'#9E3B33'
+					backgroundColor:color.red
 				},
 				{
+					id: 5,
 					title: 'Lunch',
 					start: new Date(y, m, d, 12, 0),
 					end: new Date(y, m, d, 14, 0),
 					backgroundColor:'#E48743'
 				},
 				{
+					id: 6,
 					title: 'Birthday Party',
 					start: new Date(y, m, d+1, 19-diff, 0),
 					end: new Date(y, m, d+2, 2-diff, 30),
