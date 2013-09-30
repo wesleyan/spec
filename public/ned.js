2707156f5d3ee6f16eb3db9b7a7235d6596550ed
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-09-30 14:13:05 -0400

    feature: inventory list, has bugs right now

--- public/ned.js
+++ public/ned.js
@@ -30,16 +30,15 @@ resizeMap = function () {
 	};
 
 
-function setTimeline(view) {
+
+function setTimeline(view) { //this is borrowed from stackoverflow
     var parentDiv = jQuery(".fc-agenda-slots:visible").parent();
     var timeline = parentDiv.children(".timeline");
     if (timeline.length == 0) { //if timeline isn't there, add it
         timeline = jQuery("<hr>").addClass("timeline");
         parentDiv.prepend(timeline);
     }
-
     var curTime = new Date();
-
     var curCalView = jQuery("#calendar").fullCalendar('getView');
     if (curCalView.visStart < curTime && curCalView.visEnd > curTime) {
         timeline.show();
@@ -47,13 +46,10 @@ function setTimeline(view) {
         timeline.hide();
         return;
     }
-
     var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
     var percentOfDay = curSeconds / 86400; //24 * 60 * 60 = 86400, # of seconds in a day
     var topLoc = Math.floor(parentDiv.height() * percentOfDay);
-
     timeline.css("top", topLoc + "px");
-
     if (curCalView.name == "agendaWeek") { //week view, don't want the timeline to go the whole way across
         var dayCol = jQuery(".fc-today:visible");
         var left = dayCol.position().left + 1;
@@ -63,7 +59,6 @@ function setTimeline(view) {
             width: width + "px"
         });
     }
-
 }
 
 Array.prototype.removeItem = function(a) {
@@ -241,6 +236,15 @@ $(document).ready(function() {
 	resizeMap();
 	$('#leftGroup').prependTo('.fc-header-left');
 	$('#rightGroup').appendTo('.fc-header-right');
+
+	$('#inventory').tagsinput({
+	  typeahead: {
+	    source: function(query) {
+	      return $.getJSON('inventory.json');
+	    }
+	  }
+	});
+
 });
 
 
