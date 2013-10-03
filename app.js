291f3229dee28e7989380d6383e70cf817a541e1
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-10-03 10:49:01 -0400

    fix: js styling

--- app.js
+++ app.js
@@ -6,115 +6,108 @@ var path = require('path');
 
 var date = new Date();
 //var diff = date.getTimezoneOffset()/60;
-var diff =0;
+var diff = 0;
 
 var d = date.getDate();
 var m = date.getMonth();
 var y = date.getFullYear();
 
 var color = {
-	'green':'#097054',
-	'red':'#9E3B33',
+	'green': '#097054',
+	'red': '#9E3B33',
 	'yellow': '#E48743',
 	'gray': '#666666'
 }
 
-var events = [
-				{
-					id: 1,
-					title: 'Luncheon: Division III NSM Luncheon',
-					desc: 'Please arrive at 11:30 am to help set up the existing projector and screen. Client will bring their own PC laptop.',
-					loc: 'ESC 184 (Woodhead Lounge)',
-					staffAdded: 1,
-					staffNeeded: 1,
-					start: new Date(y, m, d, 9-diff, 45),
-					end: new Date(y, m, d, 13-diff, 30),
-					valid: true,
-					duration: true,
-					people: ['ckorkut']
-				},
-				{
-					id: 2,
-					title: 'Reception: Prof. F. Reeve Memorial & Reception',
-					desc: 'Client would like technician to arrive at 1pm to set up laptop to play CDs during the reception. Client would like to have the technician on hand for two hours. Please bring handheld wireless microphone.',
-					loc: 'Russell House All Rooms ',
-					staffAdded: 1,
-					staffNeeded: 1,
-					start: new Date(y, m, d, 13-diff, 0),
-					end: new Date(y, m, d, 18-diff, 0),
-					valid: true,
-					duration: false,className:['striped'],
-					people: ['tskim']
-				},
-				{
-					id: 3,
-					title: 'Dance Presentation',
-					desc: 'A technician is needed to stay for the duration of the presentation to assist with hooking up a laptop to the ceiling projector to play youtube clips and other things. NOTE: This is for a class so there should be no charge.',
-					loc: 'Schonberg Dance Studio',
-					staffAdded: 2,
-					staffNeeded: 2,
-					start: new Date(y, m, d, 13-diff, 15),
-					end: new Date(y, m, d, 14-diff, 30),
-					valid: true,
-					video: true,
-					duration: true,
-					people: ['jgoh','ckorkut']
-				},
-				{
-					id: 4,
-					title: 'Meeting',
-					desc:'Please have Heric Flores present on site for the duration of the meeting to serve as AV technician.',
-					loc: 'Shanklin 107 (Kerr Lecture Hall)',
-					staffAdded: 1,
-					staffNeeded: 1,
-					start: new Date(y, m, d+1, 10-diff, 30),
-					valid: false,
-					duration: true,
-					people: ['hflores']
-				},
-				{
-					id: 5,
-					title: 'Lunch',
-					desc:'Please have Heric Flores present on site at 8am and remain for the duration of the meeting to serve as AV technician. Presenter will arive at 8:30am and will bring laptop. Please connect presenter`s laptop to projection and project onto the screen on the west wall. Provide lavaliere mic for presenter.',
-					loc: 'Usdan 300 (Daniel Family Commons & Lounge)',
-					staffAdded: 1,
-					staffNeeded: 2,
-					start: new Date(y, m, d, 12, 0),
-					end: new Date(y, m, d, 14, 0),
-					valid: true,
-					duration: false,className:['striped'],
-					people: ['hflores']
-				},
-				{
-					id: 6,
-					title: 'Birthday Party',
-					desc:'Please arrive at 19:00 pm (promptly) to set up the existing computer, projector, and screen. Please plan to stay for the entire event to troubleshoot if necessary.',
-					loc: 'Beckham Hall',
-					staffAdded: 0,
-					staffNeeded: 1,
-					start: new Date(y, m, d+1, 19-diff, 0),
-					end: new Date(y, m, d+2, 2-diff, 30),
-					valid: true,
-					duration: true,
-					people: []
-					//url: 'http://google.com/'
-				}
-			];
-
+var events = [{
+	id: 1,
+	title: 'Luncheon: Division III NSM Luncheon',
+	desc: 'Please arrive at 11:30 am to help set up the existing projector and screen. Client will bring their own PC laptop.',
+	loc: 'ESC 184 (Woodhead Lounge)',
+	staffAdded: 1,
+	staffNeeded: 1,
+	start: new Date(y, m, d, 9 - diff, 45),
+	end: new Date(y, m, d, 13 - diff, 30),
+	valid: true,
+	duration: true,
+	people: ['ckorkut']
+}, {
+	id: 2,
+	title: 'Reception: Prof. F. Reeve Memorial & Reception',
+	desc: 'Client would like technician to arrive at 1pm to set up laptop to play CDs during the reception. Client would like to have the technician on hand for two hours. Please bring handheld wireless microphone.',
+	loc: 'Russell House All Rooms ',
+	staffAdded: 1,
+	staffNeeded: 1,
+	start: new Date(y, m, d, 13 - diff, 0),
+	end: new Date(y, m, d, 18 - diff, 0),
+	valid: true,
+	duration: false,
+	className: ['striped'],
+	people: ['tskim']
+}, {
+	id: 3,
+	title: 'Dance Presentation',
+	desc: 'A technician is needed to stay for the duration of the presentation to assist with hooking up a laptop to the ceiling projector to play youtube clips and other things. NOTE: This is for a class so there should be no charge.',
+	loc: 'Schonberg Dance Studio',
+	staffAdded: 2,
+	staffNeeded: 2,
+	start: new Date(y, m, d, 13 - diff, 15),
+	end: new Date(y, m, d, 14 - diff, 30),
+	valid: true,
+	video: true,
+	duration: true,
+	people: ['jgoh', 'ckorkut']
+}, {
+	id: 4,
+	title: 'Meeting',
+	desc: 'Please have Heric Flores present on site for the duration of the meeting to serve as AV technician.',
+	loc: 'Shanklin 107 (Kerr Lecture Hall)',
+	staffAdded: 1,
+	staffNeeded: 1,
+	start: new Date(y, m, d + 1, 10 - diff, 30),
+	valid: false,
+	duration: true,
+	people: ['hflores']
+}, {
+	id: 5,
+	title: 'Lunch',
+	desc: 'Please have Heric Flores present on site at 8am and remain for the duration of the meeting to serve as AV technician. Presenter will arive at 8:30am and will bring laptop. Please connect presenter`s laptop to projection and project onto the screen on the west wall. Provide lavaliere mic for presenter.',
+	loc: 'Usdan 300 (Daniel Family Commons & Lounge)',
+	staffAdded: 1,
+	staffNeeded: 2,
+	start: new Date(y, m, d, 12, 0),
+	end: new Date(y, m, d, 14, 0),
+	valid: true,
+	duration: false,
+	className: ['striped'],
+	people: ['hflores']
+}, {
+	id: 6,
+	title: 'Birthday Party',
+	desc: 'Please arrive at 19:00 pm (promptly) to set up the existing computer, projector, and screen. Please plan to stay for the entire event to troubleshoot if necessary.',
+	loc: 'Beckham Hall',
+	staffAdded: 0,
+	staffNeeded: 1,
+	start: new Date(y, m, d + 1, 19 - diff, 0),
+	end: new Date(y, m, d + 2, 2 - diff, 30),
+	valid: true,
+	duration: true,
+	people: []
+	//url: 'http://google.com/'
+}];
 
 
 
 function addBackgroundColor(events) { //changes the events object
 	for (index = 0; index < events.length; ++index) {
 		event = events[index];
-		if(event.valid == false) {
+		if (event.valid == false) {
 			events[index]['backgroundColor'] = color.gray;
-		} else if(event.staffAdded == 0) {
+		} else if (event.staffAdded == 0) {
 			events[index]['backgroundColor'] = color.red;
-		}
-		else if(event.staffAdded < event.staffNeeded) {
+		} else if (event.staffAdded < event.staffNeeded) {
 			events[index]['backgroundColor'] = color.yellow;
-		} else if(event.staffAdded == event.staffNeeded) {
+		} else if (event.staffAdded == event.staffNeeded) {
 			events[index]['backgroundColor'] = color.green;
 		}
 	}
@@ -122,86 +115,84 @@ function addBackgroundColor(events) { //changes the events object
 }
 events = addBackgroundColor(events);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 
-app.get("/events", function(req,res) {
+app.get("/events", function(req, res) {
 	//86400s = 1d
-	var start = new Date(req.query.start*1000);
-	var end = new Date(req.query.end*1000);
+	var start = new Date(req.query.start * 1000);
+	var end = new Date(req.query.end * 1000);
 	//req.url
-    console.log("Req for events starting at " + start.toDateString() + " and ending before " + end.toDateString());
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(events).toString("utf-8"));
-    res.end();
+	console.log("Req for events starting at " + start.toDateString() + " and ending before " + end.toDateString());
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	res.write(JSON.stringify(events).toString("utf-8"));
+	res.end();
 });
 
-var allInventory = [
-    {
-        "id": "4",
-        "text": "Video Camera",
-        "title": "This item needs to be recorded."
-    },
-    {
-        "id": "3",
-        "text": "Camera",
-        "title": "This item needs to be recorded."
-    },
-    {
-        "id": "3",
-        "text": "Tripod",
-        "title": "This item needs to be recorded."
-    },
-    {
-        "id": "5",
-        "text": "HDMI Cable",
-        "title": "This item needs to be recorded."
-    },
-    {
-        "id": "1",
-        "text": "Projector",
-        "title": "This item needs to be recorded."
-    },
+var allInventory = [{
+		"id": "4",
+		"text": "Video Camera",
+		"title": "This item needs to be recorded."
+	}, {
+		"id": "3",
+		"text": "Camera",
+		"title": "This item needs to be recorded."
+	}, {
+		"id": "3",
+		"text": "Tripod",
+		"title": "This item needs to be recorded."
+	}, {
+		"id": "5",
+		"text": "HDMI Cable",
+		"title": "This item needs to be recorded."
+	}, {
+		"id": "1",
+		"text": "Projector",
+		"title": "This item needs to be recorded."
+	},
 
-    {
-        "id": "2",
-        "text": "Macbook Pro 13",
-        "title": "This item needs to be recorded."
-    },
-    {
-        "id": "3",
-        "text": "iMac 21.5",
-        "title": "This item needs to be recorded."
-    }
+	{
+		"id": "2",
+		"text": "Macbook Pro 13",
+		"title": "This item needs to be recorded."
+	}, {
+		"id": "3",
+		"text": "iMac 21.5",
+		"title": "This item needs to be recorded."
+	}
 ];
-var existingInventory = [
-    {
-        "id": "2",
-        "text": "Macbook Pro 13",
-        "title": "This item needs to be recorded."
-    }
-];
+var existingInventory = [{
+	"id": "4",
+	"text": "Video Camera",
+	"title": "This item needs to be recorded."
+}];
 
-app.get("/inventory/all", function(req,res) {
+app.get("/inventory/all", function(req, res) {
 	//req.url
-    console.log("Req for inventory");
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(allInventory).toString("utf-8"));
-    res.end();
+	console.log("Req for inventory");
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	res.write(JSON.stringify(allInventory).toString("utf-8"));
+	res.end();
 });
-app.get("/inventory/existing", function(req,res) {
+app.get("/inventory/existing", function(req, res) {
 	//req.url
-    console.log("Req for inventory");
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(existingInventory).toString("utf-8"));
-    res.end();
+	console.log("Req for inventory");
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	res.write(JSON.stringify(existingInventory).toString("utf-8"));
+	res.end();
 });
 
 /*app.get('/', function (req, res) {
@@ -210,12 +201,9 @@ app.get("/inventory/existing", function(req,res) {
   )
 });*/
 
-app.get('/printtoday', function (req, res) {
-  res.render('printtoday'
-  )
+app.get('/printtoday', function(req, res) {
+	res.render('printtoday')
 });
-
-
 
 app.listen(8080);
 console.log('Listening on port 8080');
--- public/ned.js
+++ public/ned.js
@@ -1,14 +1,13 @@
 function changePopupColor(event) { //changes the events object
 	$("#popupTitleButton").removeClass("btn-success btn-inverse btn-warning btn-danger");
-	if(event.valid == false) {
-			$("#popupTitleButton").addClass("btn-inverse");
-		} else if(event.staffAdded == 0) {
-			$("#popupTitleButton").addClass("btn-danger");
-		}
-		else if(event.staffAdded < event.staffNeeded) {
-			$("#popupTitleButton").addClass("btn-warning");
-		} else if(event.staffAdded == event.staffNeeded) {
-			$("#popupTitleButton").addClass("btn-success");
+	if (event.valid == false) {
+		$("#popupTitleButton").addClass("btn-inverse");
+	} else if (event.staffAdded == 0) {
+		$("#popupTitleButton").addClass("btn-danger");
+	} else if (event.staffAdded < event.staffNeeded) {
+		$("#popupTitleButton").addClass("btn-warning");
+	} else if (event.staffAdded == event.staffNeeded) {
+		$("#popupTitleButton").addClass("btn-success");
 	}
 }
 
@@ -17,61 +16,61 @@ $('#eventButton').click(function(e) {
 	$('#popup').modalPopover('hide');
 });
 
-$('.modal').on('show', function () {
-  $('#popup').css('opacity', 0.7);
-}).on('hide', function () {
-  $('#popup').css('opacity', 1);
+$('.modal').on('show', function() {
+	$('#popup').css('opacity', 0.7);
+}).on('hide', function() {
+	$('#popup').css('opacity', 1);
 });
 
-resizeMap = function () {
-		var column_height = $(window).height();
-		$('#calendar').fullCalendar('option', 'height', column_height - 40);
-		//$("#calendar").css("height", + "px")
-	};
+resizeMap = function() {
+	var column_height = $(window).height();
+	$('#calendar').fullCalendar('option', 'height', column_height - 40);
+	//$("#calendar").css("height", + "px")
+};
 
 
 
 function setTimeline(view) { //this is borrowed from stackoverflow
-    var parentDiv = jQuery(".fc-agenda-slots:visible").parent();
-    var timeline = parentDiv.children(".timeline");
-    if (timeline.length == 0) { //if timeline isn't there, add it
-        timeline = jQuery("<hr>").addClass("timeline");
-        parentDiv.prepend(timeline);
-    }
-    var curTime = new Date();
-    var curCalView = jQuery("#calendar").fullCalendar('getView');
-    if (curCalView.visStart < curTime && curCalView.visEnd > curTime) {
-        timeline.show();
-    } else {
-        timeline.hide();
-        return;
-    }
-    var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
-    var percentOfDay = curSeconds / 86400; //24 * 60 * 60 = 86400, # of seconds in a day
-    var topLoc = Math.floor(parentDiv.height() * percentOfDay);
-    timeline.css("top", topLoc + "px");
-    if (curCalView.name == "agendaWeek") { //week view, don't want the timeline to go the whole way across
-        var dayCol = jQuery(".fc-today:visible");
-        var left = dayCol.position().left + 1;
-        var width = dayCol.width()-2;
-        timeline.css({
-            left: left + "px",
-            width: width + "px"
-        });
-    }
+	var parentDiv = jQuery(".fc-agenda-slots:visible").parent();
+	var timeline = parentDiv.children(".timeline");
+	if (timeline.length == 0) { //if timeline isn't there, add it
+		timeline = jQuery("<hr>").addClass("timeline");
+		parentDiv.prepend(timeline);
+	}
+	var curTime = new Date();
+	var curCalView = jQuery("#calendar").fullCalendar('getView');
+	if (curCalView.visStart < curTime && curCalView.visEnd > curTime) {
+		timeline.show();
+	} else {
+		timeline.hide();
+		return;
+	}
+	var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
+	var percentOfDay = curSeconds / 86400; //24 * 60 * 60 = 86400, # of seconds in a day
+	var topLoc = Math.floor(parentDiv.height() * percentOfDay);
+	timeline.css("top", topLoc + "px");
+	if (curCalView.name == "agendaWeek") { //week view, don't want the timeline to go the whole way across
+		var dayCol = jQuery(".fc-today:visible");
+		var left = dayCol.position().left + 1;
+		var width = dayCol.width() - 2;
+		timeline.css({
+			left: left + "px",
+			width: width + "px"
+		});
+	}
 }
 
 Array.prototype.removeItem = function(a) {
-            for (i = 0; i < this.length; i++) {
-                if (this[i] == a) {
-                    for (i2 = i; i2 < this.length - 1; i2++) {
-                        this[i2] = this[i2 + 1];
-                    }
-                    this.length = this.length - 1
-                    return;
-                }
-            }
-        }
+	for (i = 0; i < this.length; i++) {
+		if (this[i] == a) {
+			for (i2 = i; i2 < this.length - 1; i2++) {
+				this[i2] = this[i2 + 1];
+			}
+			this.length = this.length - 1
+			return;
+		}
+	}
+}
 
 function dropdownActiveFix() {
 	$('a').removeClass('drop-active');
@@ -85,29 +84,29 @@ var lastClick, lastRightClick;
 
 // BACKBONE.JS ROUTER SECTION
 var AppRouter = Backbone.Router.extend({
-  routes: {
-  	"printToday": "printToday",
-    "recentVideo": "recentVideo",
-    "*filter": "all"
-  }
+	routes: {
+		"printToday": "printToday",
+		"recentVideo": "recentVideo",
+		"*filter": "all"
+	}
 });
 
 
 //User info must be imported for this part
 var username = 'ckorkut';
-      
+
 var app = new AppRouter;
 
 app.on('route:printToday', function() {
-  	console.log('printToday');
+	console.log('printToday');
 });
 app.on('route:recentVideo', function() {
-  	console.log('recentVideo');
+	console.log('recentVideo');
 });
 
 app.on('route:all', function(filter) {
 	dropdownActiveFix();
-	if(filter == null) {
+	if (filter == null) {
 		//Show all of events
 		$('#calendar').fullCalendar('clientEvents', function(event) {
 			event.className.removeItem('hide');
@@ -115,29 +114,29 @@ app.on('route:all', function(filter) {
 		});
 		console.log('all');
 
-	} else if(filter == 'hideCancelled') {
-		
+	} else if (filter == 'hideCancelled') {
+
 		$('#calendar').fullCalendar('clientEvents', function(event) {
 			event.className.removeItem('hide');
-			if(event.valid == false) {
+			if (event.valid == false) {
 				event.className.push('hide');
 			}
 			$('#calendar').fullCalendar('updateEvent', event);
 		});
 		console.log(filter);
-	} else if(filter == 'unstaffed') {
+	} else if (filter == 'unstaffed') {
 		$('#calendar').fullCalendar('clientEvents', function(event) {
 			event.className.removeItem('hide');
-			if(event.staffAdded == event.staffNeeded || event.valid == false) {
+			if (event.staffAdded == event.staffNeeded || event.valid == false) {
 				event.className.push('hide');
 			}
 			$('#calendar').fullCalendar('updateEvent', event);
 		});
 		console.log(filter);
-	} else if(filter == 'onlyMine') {
+	} else if (filter == 'onlyMine') {
 		$('#calendar').fullCalendar('clientEvents', function(event) {
 			event.className.removeItem('hide');
-			if(event.people.indexOf(username) == -1) {
+			if (event.people.indexOf(username) == -1) {
 				event.className.push('hide');
 			}
 			$('#calendar').fullCalendar('updateEvent', event);
@@ -161,21 +160,21 @@ $('a[href="#staffEvent"]').click(function(e) {
 
 // Solves Bootstrap typeahead dropdown problem
 $('.collapse').on({
-    hide: function(){
-       $(this).css('overflow','hidden');
+	hide: function() {
+		$(this).css('overflow', 'hidden');
 
-       //$(this).hide();
-    },
-    click: function() {
-		$('.collapse').css('overflow','visible');
+		//$(this).hide();
+	},
+	click: function() {
+		$('.collapse').css('overflow', 'visible');
 	},
 	shown: function() {
-		$('.collapse').css('overflow','visible');
+		$('.collapse').css('overflow', 'visible');
 	}
 });
-$('button[data-target="#viewdetails"]').click(function(){
-        $(this).toggleClass("active");
-    });
+$('button[data-target="#viewdetails"]').click(function() {
+	$(this).toggleClass("active");
+});
 
 $(document).ready(function() {
 
@@ -184,11 +183,11 @@ $(document).ready(function() {
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
-			$('#popup').modalPopover({
-			    target: '#eventButton',
-			    placement: 'bottom',
-			    backdrop: false
-			});	
+	$('#popup').modalPopover({
+		target: '#eventButton',
+		placement: 'bottom',
+		backdrop: false
+	});
 
 	$('#calendar').fullCalendar({
 		header: {
@@ -203,14 +202,14 @@ $(document).ready(function() {
 		firstDay: date.getDay(),
 		eventBorderColor: 'black',
 		windowResize: function(view) {
-	        resizeMap();
-	    },
+			resizeMap();
+		},
 		eventClick: function(calEvent, jsEvent, view) {
 			symbol = '';
-			if(calEvent.video == true) {
-				symbol += '<i class="icon-facetime-video"></i> ';  
-			} 
-		  //This function should contain specific stuff like opening the event-based selection/description box etc
+			if (calEvent.video == true) {
+				symbol += '<i class="icon-facetime-video"></i> ';
+			}
+			//This function should contain specific stuff like opening the event-based selection/description box etc
 			$('#popup').modalPopover('show');
 			$('#eventButton').removeClass('disabled');
 			$('#popupTitle').html(symbol + calEvent.title);
@@ -221,59 +220,65 @@ $(document).ready(function() {
 			lastClickedEvent = calEvent;
 		},
 		eventRightClick: function(calEvent, jsEvent, view) {
-		  jsEvent.preventDefault(); //Right click event only prevents default because context menu is binded in eventRender
-		  lastClickedEvent = calEvent;
+			jsEvent.preventDefault(); //Right click event only prevents default because context menu is binded in eventRender
+			lastClickedEvent = calEvent;
 		},
 		eventRender: function(event, element) {
 			//Adding all events to an array for event filtering with Backbone.js router
 			symbol = '';
-			if(event.video == true) {
-				symbol += '<i class="icon-facetime-video icon-white"></i> ';  
-			}                                   
+			if (event.video == true) {
+				symbol += '<i class="icon-facetime-video icon-white"></i> ';
+			}
 			element.find('.fc-event-title').html(symbol + event.title);
-			element.contextmenu({'target':'#context-menu'});
-	  
+			element.contextmenu({
+				'target': '#context-menu'
+			});
+
 		},
 		viewRender: function(view, element) {
-		 	//console.log(view.name);
-	        try {
-	            setTimeline();
-	        } catch(err) {}
-	    },
-	    newEventsComplete: function() { //after each ajax request to the server, new events also filtered by this way
-	    	var currentUrl = Backbone.history.fragment;
-	    	if(currentUrl != '') {
-	    		app.navigate('', {trigger: true});
-	    		app.navigate(currentUrl, {trigger: true});
-	    	}
-	    },
-	    eventSources: [{
-	        url: 'events/', // Shows all events BUT need it to show only events to certain location
-	        ignoreTimezone: false
-	    }],
+			//console.log(view.name);
+			try {
+				setTimeline();
+			} catch (err) {}
+		},
+		newEventsComplete: function() { //after each ajax request to the server, new events also filtered by this way
+			var currentUrl = Backbone.history.fragment;
+			if (currentUrl != '') {
+				app.navigate('', {
+					trigger: true
+				});
+				app.navigate(currentUrl, {
+					trigger: true
+				});
+			}
+		},
+		eventSources: [{
+			url: 'events/', // Shows all events BUT need it to show only events to certain location
+			ignoreTimezone: false
+		}],
 	});
 
 	//Important: especially not using defaultView option of FullCalendar, for efficient use of lazyFetching.
-	$('#calendar').fullCalendar('changeView', 'agendaWeek'); 
+	$('#calendar').fullCalendar('changeView', 'agendaWeek');
 	//Loads the whole month events, and shows them from memory, instead of a new request for each prev/next click.
 	resizeMap();
 	$('#leftGroup').prependTo('.fc-header-left');
 	$('#rightGroup').appendTo('.fc-header-right');
 
 	$('#timepicker5').timepicker({
-            template: false,
-            showInputs: false,
-            minuteStep: 5,
-            defaultTime: '9:45 AM'
-    });
+		template: false,
+		showInputs: false,
+		minuteStep: 5,
+		defaultTime: '9:45 AM'
+	});
 	$('#timepicker6').timepicker({
-            template: false,
-            showInputs: false,
-            minuteStep: 5,
-            defaultTime: '11:45 AM'
-    });
+		template: false,
+		showInputs: false,
+		minuteStep: 5,
+		defaultTime: '11:45 AM'
+	});
 	$('.combobox').combobox();
-	
+
 	/*$('#inventory').typeahead({
 		source: function(query, process) {
 	        jQuery.ajax({
@@ -292,24 +297,20 @@ $(document).ready(function() {
 		values_url: 'inventory/existing',
 
 		//These methods below have to send AJAX requests to update the inventory.
-		onRemove: function(pill){
-	        var id = pill.data('tag-id');
-	        // do something...
-	        console.log('pill with ID ' + id + ' removed');
-	    },
-	   	onBeforeAdd: function(pill){ //
-	        var id = pill.data('tag-id');
-	        // do something...
-	        console.log('pill with ID ' + id + ' added');
-	        return pill; //has to return pill
-	    }
+		onRemove: function(pill) {
+			var id = pill.data('tag-id');
+			// do something...
+			console.log('pill with ID ' + id + ' removed');
+		},
+		onBeforeAdd: function(pill) { //
+			var id = pill.data('tag-id');
+			// do something...
+			console.log('pill with ID ' + id + ' added');
+			return pill; //has to return pill
+		}
 	});
-//should look for ways to modify inventory tags because recalling the method just appends. maybe recreating this for each event click.
+	//should look for ways to modify inventory tags because recalling the method just appends. maybe recreating this for each event click.
 
 
 
 });
-
-
-
-
