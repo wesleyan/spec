2b3b8d5bb2bf97f5e6db5b1a9e5dfb45ee679721
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-10-15 12:45:42 -0400

    feature: x-editable integration, form design to be done

--- public/ned.js
+++ public/ned.js
@@ -154,6 +154,29 @@ _.templateSettings.variable = "op";
 Spec.View.Edit = Backbone.View.extend({
         initialize: function(){
             this.render();
+            //toggle `popup` / `inline` mode
+		    $.fn.editable.defaults.mode = 'inline';     
+		    
+		    //make username editable
+		    $('#username').editable();
+		    
+		    //make status editable
+		    $('#status').editable({
+		        type: 'select',
+		        title: 'Select status',
+		        placement: 'right',
+		        value: 2,
+		        source: [
+		            {value: 1, text: 'status 1'},
+		            {value: 2, text: 'status 2'},
+		            {value: 3, text: 'status 3'}
+		        ]
+		        /*
+		        //uncomment these lines to send data on server
+		        ,pk: 1
+		        ,url: '/post'
+		        */
+		    });
         },
         render: function(){
             var variables = { event: Spec.lastClickedEvent};
