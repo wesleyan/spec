565fa994c004d4b9fdfd4b637bf1e856c13222ba
Cumhur Korkut <cumhurkorkut@gmail.com>
2013-11-22 14:41:21 -0500

    closes #1

--- sampleEvents.js
+++ sampleEvents.js
@@ -10,17 +10,17 @@ var d = date.getDate();
 var m = date.getMonth();
 var y = date.getFullYear();
 
-//staff added is also needless. other than that, inventory, people and shifts changed, keep in mind for front end
 var events = [{
 	title: 'Luncheon: Division III NSM Luncheon',
 	desc: 'Please arrive at 11:30 am to help set up the existing projector and screen. Client will bring their own PC laptop.',
 	loc: 'ESC 184 (Woodhead Lounge)',
-	staffAdded: 1,
 	staffNeeded: 1,
-	start: new Date(y, m, d, 9 - diff, 45),
-	end: new Date(y, m, d, 13 - diff, 30),
-	valid: true,
-	duration: true,
+	start: new Date(y, m, d, 9 - diff, 45),			//'start' means the tech reserved/setup time start
+	end: new Date(y, m, d, 13 - diff, 30),			//'end' means the tech reserved/setup time end
+	eventStart: new Date(y, m, d, 9 - diff, 45),
+	eventEnd: new Date(y, m, d, 13 - diff, 30),
+	cancelled: false,
+	techMustStay: true,
 	inventory: [4],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -44,12 +44,13 @@ var events = [{
 	title: 'Reception: Prof. F. Reeve Memorial & Reception',
 	desc: 'Client would like technician to arrive at 1pm to set up laptop to play CDs during the reception. Client would like to have the technician on hand for two hours. Please bring handheld wireless microphone.',
 	loc: 'Russell House All Rooms ',
-	staffAdded: 1,
 	staffNeeded: 1,
 	start: new Date(y, m, d, 13 - diff, 0),
 	end: new Date(y, m, d, 18 - diff, 0),
-	valid: true,
-	duration: false,
+	eventStart: new Date(y, m, d, 13 - diff, 0),
+	eventEnd: new Date(y, m, d, 18 - diff, 0),
+	cancelled: false,
+	techMustStay: false,
 	inventory: [2],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -73,13 +74,14 @@ var events = [{
 	title: 'Dance Presentation',
 	desc: 'A technician is needed to stay for the duration of the presentation to assist with hooking up a laptop to the ceiling projector to play youtube clips and other things. NOTE: This is for a class so there should be no charge.',
 	loc: 'Schonberg Dance Studio',
-	staffAdded: 2,
 	staffNeeded: 2,
 	start: new Date(y, m, d, 13 - diff, 15),
 	end: new Date(y, m, d, 14 - diff, 30),
-	valid: true,
+	eventStart: new Date(y, m, d, 13 - diff, 15),
+	eventEnd: new Date(y, m, d, 14 - diff, 30),
+	cancelled: false,
 	video: true,
-	duration: true,
+	techMustStay: true,
 	inventory: [4, 6],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -109,12 +111,13 @@ var events = [{
 	title: 'Meeting',
 	desc: 'Please have Heric Flores present on site for the duration of the meeting to serve as AV technician.',
 	loc: 'Shanklin 107 (Kerr Lecture Hall)',
-	staffAdded: 1,
 	staffNeeded: 1,
 	start: new Date(y, m, d + 1, 10 - diff, 30),
 	end: new Date(y, m, d + 1, 12 - diff, 30),
-	valid: false,
-	duration: true,
+	eventStart: new Date(y, m, d + 1, 10 - diff, 30),
+	eventEnd: new Date(y, m, d + 1, 12 - diff, 30),
+	cancelled: true,
+	techMustStay: true,
 	inventory: [4],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -138,12 +141,13 @@ var events = [{
 	title: 'Lunch',
 	desc: 'Please have Heric Flores present on site at 8am and remain for the duration of the meeting to serve as AV technician. Presenter will arive at 8:30am and will bring laptop. Please connect presenter`s laptop to projection and project onto the screen on the west wall. Provide lavaliere mic for presenter.',
 	loc: 'Usdan 300 (Daniel Family Commons & Lounge)',
-	staffAdded: 1,
 	staffNeeded: 2,
 	start: new Date(y, m, d, 12, 0),
 	end: new Date(y, m, d, 14, 0),
-	valid: true,
-	duration: false,
+	eventStart: new Date(y, m, d, 12, 0),
+	eventEnd: new Date(y, m, d, 14, 0),
+	cancelled: false,
+	techMustStay: false,
 	inventory: [1],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -167,12 +171,13 @@ var events = [{
 	title: 'Birthday Party',
 	desc: 'Please arrive at 19:00 pm (promptly) to set up the existing computer, projector, and screen. Please plan to stay for the entire event to troubleshoot if necessary.',
 	loc: 'Beckham Hall',
-	staffAdded: 0,
 	staffNeeded: 1,
 	start: new Date(y, m, d + 1, 19 - diff, 0),
 	end: new Date(y, m, d + 2, 2 - diff, 30),
-	valid: true,
-	duration: true,
+	eventStart: new Date(y, m, d + 1, 19 - diff, 0),
+	eventEnd: new Date(y, m, d + 2, 2 - diff, 30),
+	cancelled: false,
+	techMustStay: true,
 	inventory: [4],
 	notes: [{
 		id: new mongo.ObjectID(),
@@ -193,36 +198,3 @@ events.forEach(function(event) {
 		else console.log("Event saved");
 	});
 });
-
-//sample inventory
-var allInventory = [
-		{
-			"id": 4,
-			"text": "Video Camera",
-			"title": "This item needs to be recorded."
-		}, {
-			"id": 6,
-			"text": "Camera",
-			"title": "This item needs to be recorded."
-		}, {
-			"id": 7,
-			"text": "Tripod",
-			"title": "This item needs to be recorded."
-		}, {
-			"id": 5,
-			"text": "HDMI Cable",
-			"title": "This item needs to be recorded."
-		}, {
-			"id": 1,
-			"text": "Projector",
-			"title": "This item needs to be recorded."
-		},	{
-			"id": 2,
-			"text": "Macbook Pro 13",
-			"title": "This item needs to be recorded."
-		}, {
-			"id": 3,
-			"text": "iMac 21.5",
-			"title": "This item needs to be recorded."
-		}
-	];
