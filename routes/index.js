bd9747d943fd8a156f38742cb2789d2219897273
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-02-21 11:41:09 -0500

    google calendar stuff into module

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 module.exports = {
-	general: 	require('./general.js'),
-	events: 	require('./events.js'),
-	api: 		require('./api.js'),
-	print: 		require('./print.js'),
+	general: 		require('./general.js'),
+	events: 		require('./events.js'),
+	api: 			require('./api.js'),
+	print: 			require('./print.js'),
 
-	staff: 		require('./staff.js'),
-	inventory: 	require('./inventory.js'),
-	notes: 		require('./notes.js')
+	staff: 			require('./staff.js'),
+	inventory: 		require('./inventory.js'),
+	notes: 			require('./notes.js'),
+
+	googleCalendar: require('./googleCalendar.js')
 };
