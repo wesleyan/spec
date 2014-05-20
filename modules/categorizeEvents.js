76f39b648482900de8683e1d58c32563056efe12
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-05-20 11:30:41 -0400

    check event descriptions for keywords

--- modules/categorizeEvents.js
+++ modules/categorizeEvents.js
@@ -1,4 +1,47 @@
+var bEventKeywords = [
+   /\bPA\b/g,
+   /\bspeakers\b/gi,
+   /\bmicrophones\b/gi,
+   /\btable top\b/gi,
+   /\btable-top\b/gi
+];
+
+
+var cEventKeywords = [
+   /\bwebcast\b/gi,
+   /\bweb-cast\b/gi,
+   /\blive streaming\b/gi,
+   /\blive-streaming\b/gi,
+   /\bbroadcast\b/gi
+];
+
+// hasKeywords(keywords, text) = true or false
+// keywords are an array of regular expressions, text is a string
+var hasKeywords = function (keywords, text) {
+  return keywords.reduceRight(function (init, regexp) {
+    return init || regexp.test(text);
+  }, false);
+};
+
+// detectCategory(event) = returns "A", "B" or "C"
+var detectCategory = function (event) {
+  event.category = "A";
+
+  //B event detection
+  if(hasKeywords(bEventKeywords, event.desc) {
+    event.category = "B";
+  }
+
+  //C event detection
+  if(hasKeywords(cEventKeywords, event.desc) {
+    event.category = "C";
+  }
+
+  return event.category;
+};
+
 module.exports = function(event) {
   event.category = "A";
+  //event.category = detectCategory(event);
 	return event;
 };
