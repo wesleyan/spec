a68237455f994cb8e9d582d8a914f3c16b8a0967
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-05-27 10:42:50 -0400

    removed events preferences

--- public/src/js/spec.js
+++ public/src/js/spec.js
@@ -738,7 +738,7 @@ var Spec = {}; //the only global variable that is supposed to be used in this ap
                 url: "event/remove",
                 data: {
                     eventid: Spec.lastClickedEvent._id,
-                    XMLid: Spec.lastClickedEvent._id
+                    XMLid: Spec.lastClickedEvent.XMLid
                 }
             }).done(function(msg) {
                 console.log('event with ID ' + Spec.lastClickedEvent._id + ' removed');
