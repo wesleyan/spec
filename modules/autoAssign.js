16d4df932fb0e644df9eba6549c1fa7988fc3052
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-05-28 14:04:31 -0400

    styling && assign the one with the least points if possible

--- modules/autoAssign.js
+++ modules/autoAssign.js
@@ -10,17 +10,17 @@ var isValidDate = function(d) {
 };
 
 var fetch = function(start, end, user, callback) {
-    fetchCalendars({
-        start: start,
-        end:   end,
-        user:  user,
-        success: function(items) {
-            callback(items);
-        },
-        error: function(err) {
-            callback([]);
-        }
-    });
+  fetchCalendars({
+    start: start,
+    end: end,
+    user: user,
+    success: function(items) {
+      callback(items);
+    },
+    error: function(err) {
+      callback([]);
+    }
+  });
 };
 
 var isStaffAvailable = function(event, pointStaffObj) {
@@ -40,10 +40,10 @@ var assignEventStaff = function(event, pointStaffObj) {
   console.log('Assign ' + pointStaffObj.staff.username + ' to ' + event.title);
 };
 
-module.exports = function () {
+module.exports = function() {
   var threeDaysLater = {
-      start: moment().add('d', 3).startOf('day').toDate(),
-      end: moment().add('d', 3).endOf('day').toDate()
+    start: moment().add('d', 3).startOf('day').toDate(),
+    end: moment().add('d', 3).endOf('day').toDate()
   };
 
   var eventsToAssign = [];
@@ -53,89 +53,104 @@ module.exports = function () {
       $gte: threeDaysLater.start,
       $lt: threeDaysLater.end
     }
-  }).toArray().then(function (events) {
+  }).toArray().then(function(events) {
     eventsToAssign = events;
-    return db.staff.find({task: 'events'}).toArray();
-  }).then(function (employees) {
-     parallel = employees.map(function (employee) {
-       return function (cb) {
-         db.events.find({
-                         start: {
-                           $gte: moment().subtract('d', 30).startOf('day').toDate()
-                         }, 
-                         shifts: {$elemMatch: {staff: employee.username}}
-                       }, function (err, events) {
-           if(err || !events) {return false;}
-           cb(null, {staff: employee, events: events});
-         });
-       };
-     });
+    return db.staff.find({
+      task: 'events',
+      professional: {
+        $exists: false //don't include professional staff
+      }
+    }).toArray();
+  }).then(function(employees) {
+    parallel = employees.map(function(employee) {
+      return function(cb) {
+        db.events.find({
+          start: {
+            $gte: moment().subtract('d', 30).startOf('day').toDate()
+          },
+          shifts: {
+            $elemMatch: {
+              staff: employee.username
+            }
+          }
+        }, function(err, events) {
+          if (err || !events) {
+            return false;
+          }
+          cb(null, {
+            staff: employee,
+            events: events
+          });
+        });
+      };
+    });
 
-     async.parallel(parallel, function (err, eventStaff) {
-       var categoryValue = {
-         "A" : 1,
-         "B" : 1.5,
-         "C" : 2
-       };
+    async.parallel(parallel, function(err, eventStaff) {
+      var categoryValue = {
+        "A": 1,
+        "B": 1.5,
+        "C": 2
+      };
 
-       var pointList = _(eventStaff.map(function (obj) {
-         var p = obj.events.reduceRight(function (point, event) {
-           return point + categoryValue[event.category];    
-         }, 0);
+      var pointList = _(eventStaff.map(function(obj) {
+        var p = obj.events.reduceRight(function(point, event) {
+          return point + categoryValue[event.category];
+        }, 0);
 
-         return {staff: obj.staff, point: p};
-       })).sortBy(function (o) {
-         return o.point;
-       });
+        return {
+          staff: obj.staff,
+          point: p
+        };
+      })).sortBy(function(o) {
+        return o.point;
+      });
 
-       //pointList is a sorted array of objects
-       //fields in objects: staff, point
-       //array asc sorted by point
+      //pointList is a sorted array of objects
+      //fields in objects: staff, point
+      //array asc sorted by point
 
-       async.map(pointList, 
-       function(pointObj, cb) {
-         //fetch events for the user and assign them to `events` key.
-         fetch(threeDaysLater.start,
-               threeDaysLater.end,
-               pointObj.staff.username, 
-               function(gEvents) {
-                 pointObj.gEvents = gEvents.map(function(gEvent) {
-                   return {
-                     start: new Date(gEvent.start.dateTime),
-                     end:   new Date(gEvent.end.dateTime)
-                   };
-                 }).filter(function(gEvent) {
-                   return isValidDate(gEvent.start) && isValidDate(gEvent.end);
-                 });
+      async.map(pointList,
+        function(pointObj, cb) {
+          //fetch events for the user and assign them to `events` key.
+          fetch(threeDaysLater.start,
+            threeDaysLater.end,
+            pointObj.staff.username,
+            function(gEvents) {
+              pointObj.gEvents = gEvents.map(function(gEvent) {
+                return {
+                  start: new Date(gEvent.start.dateTime),
+                  end: new Date(gEvent.end.dateTime)
+                };
+              }).filter(function(gEvent) {
+                return isValidDate(gEvent.start) && isValidDate(gEvent.end);
+              });
 
-                 cb(null, pointObj);
-               });
-       },
-       function(err, pointListWithEvents) {
-       //pointListWithEvents is a sorted array of objects
-       //fields in objects: staff, point, gEvents
-      
-        // Starting from the top of the list,
-        // 1) check availability for the user for each event
-        // 2) assign staff to the first possible event
-        // 3) remove the assigned staff from the candidate list
-        // 4) Continue looping through the events
-         eventsToAssign.forEach(function(event) {
-           //using a for loop to be able to break it when a match is found
-           for (var i = pointListWithEvents.length - 1; i >= 0; i--) {
-             if(isStaffAvailable(event, pointListWithEvents[i])) {
-               //staff is available, assign
-               assignEventStaff(event, pointListWithEvents[i]);
-               //remove the staff from the candidate list for next events
-               pointListWithEvents = _.without(pointListWithEvents, pointListWithEvents[i]);
-               break;
-             }
-           }
+              cb(null, pointObj);
+            });
+        },
+        function(err, pointListWithEvents) {
+          //pointListWithEvents is a sorted array of objects
+          //fields in objects: staff, point, gEvents
 
-         });
-
-       });
+          // Starting from the top of the list,
+          // 1) check availability for the user for each event
+          // 2) assign staff to the first possible event
+          // 3) remove the assigned staff from the candidate list
+          // 4) Continue looping through the events
+          eventsToAssign.forEach(function(event) {
+            //using a for loop to be able to break it when a match is found
+            for (var i = 0; i < pointListWithEvents.length; i++) {
+              if (isStaffAvailable(event, pointListWithEvents[i])) {
+                //staff is available, assign
+                assignEventStaff(event, pointListWithEvents[i]);
+                //remove the staff from the candidate list for next events
+                pointListWithEvents = _.without(pointListWithEvents, pointListWithEvents[i]);
+                break;
+              }
+            }
+          });
+        });
       //end of async.map
-     });
+    });
   });
 };
