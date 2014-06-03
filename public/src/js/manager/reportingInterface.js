8c6f0d427ce8d4476d9d6b95c259a682c3824b54
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-06-03 14:39:41 -0400

    report: event filtering, columns etc.

--- public/src/js/manager/reportingInterface.js
+++ public/src/js/manager/reportingInterface.js
@@ -4,17 +4,24 @@ var Event = Backbone.Model.extend({
 
 var PageableEventList = Backbone.PageableCollection.extend({
   model: Event,
-  url: "/events?start=1388034000&end=1391662800",
   state: {
     pageSize: 20
   },
   mode: "client"
 });
 
-var pageableEventList = new PageableEventList();
-
-pageableEventList.comparator = "title";
-pageableEventList.sort();
+var StaffNumberCell = Backgrid.Cell.extend({
+    render: function() {
+      this.$el.html(this.model.get('shifts').map(function(s){return s.staff;}).filter(function(n){return n;}).length);
+      return this;
+    }
+});
+var InventoryCell = Backgrid.Cell.extend({
+    render: function() {
+      this.$el.html(this.model.get('inventory').reduce(function(prev, current) {return prev + current.amt;}, 0));
+      return this;
+    }
+});
 
 var columns = [{
   name: "",
@@ -25,7 +32,6 @@ var columns = [{
   name: "title",
   label: "Title",
   cell: "string",
-  direction: "ascending",
   editable: false
 }, {
   name: "category",
@@ -42,37 +48,95 @@ var columns = [{
   label: "Date",
   cell: "date",
   editable: false
+}, {
+  name: "shifts",
+  label: "Staff Assigned",
+  cell: StaffNumberCell,
+  editable: false
+}, {
+  name: "staffNeeded",
+  label: "Staff Needed",
+  cell: "string",
+  editable: false
+}, {
+  name: "inventory",
+  label: "Inventory",
+  cell: InventoryCell,
+  editable: false
+}, {
+  name: "cancelled",
+  label: "Cancelled",
+  cell: "boolean",
+  editable: false
+}, {
+  name: "techMustStay",
+  label: "Tech Must Stay",
+  cell: "boolean",
+  editable: false
+}, {
+  name: "video",
+  label: "Video",
+  cell: "boolean",
+  editable: false
+}, {
+  name: "audio",
+  label: "Audio",
+  cell: "boolean",
+  editable: false
 }];
-var pageableGrid = new Backgrid.Grid({
-  columns: columns,
-  collection: pageableEventList
+
+
+
+$(document).ready(function() {
+    var today = new Date();
+    today.setHours(0,0,0,0);
+    var lastWeek = new Date(today.getTime() - 24 * 60 * 60 * 1000 * 7);
+
+    $('.date').datepicker();
+    $('#d1').datepicker('setValue', lastWeek);
+    $('#d2').datepicker('setValue', today);
+
+    pageableEventList = new PageableEventList();
+
+    var refreshEvents = function() {
+      pageableEventList.url = '/events?filter=' + $('#filter').val() + 
+                              '&start=' + ($('#d1').data('datepicker').date.getTime() / 1000) + 
+                              '&end='   + ($('#d2').data('datepicker').date.getTime() / 1000);
+      pageableEventList.fetch({
+        reset: true,
+        success: function() {$( "a:contains('Date')" ).trigger('click').trigger('click');}
+      });
+    };
+
+    $('#filter').change(refreshEvents);
+    $('.date').on('changeDate', refreshEvents);
+
+    var paginator = new Backgrid.Extension.Paginator({
+      collection: pageableEventList
+    });
+
+    var pageableGrid = new Backgrid.Grid({
+      columns: columns,
+      collection: pageableEventList
+    });
+
+    $stuff = $(".stuff");
+    $stuff.append(pageableGrid.render().el);
+    $stuff.after(paginator.render().el);
+
+    var filter = new Backgrid.Extension.ClientSideFilter({
+      collection: pageableEventList,
+      fields: ['title', 'loc']
+    });
+
+    $stuff.before(filter.render().el);
+    $(filter.el).css({
+      float: "right",
+      margin: "20px"
+    });
+    
+    refreshEvents();
 });
-
-$stuff = $(".stuff");
-$stuff.append(pageableGrid.render().el);
-
-var paginator = new Backgrid.Extension.Paginator({
-  collection: pageableEventList
-});
-
-$stuff.after(paginator.render().el);
-
-var filter = new Backgrid.Extension.ClientSideFilter({
-  collection: pageableEventList,
-  fields: ['name']
-});
-
-$stuff.before(filter.render().el);
-$(filter.el).css({
-  float: "right",
-  margin: "20px"
-});
-
-pageableEventList.fetch({
-  reset: true
-});
-
-
 $(document).keydown(function(e) {
     if (!$(event.target).is(':not(input, textarea)')) { return; } //don't do anything if on input/textarea
     switch (e.keyCode) {
