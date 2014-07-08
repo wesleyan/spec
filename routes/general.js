5b57768c7a0ecbd9c2b29a2c23cba8600331d85f
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-07-08 16:00:48 -0400

    Hotfix for the infamous bug of file download on login :clap: :muscle: :joy_cat:

--- routes/general.js
+++ routes/general.js
@@ -1,6 +1,6 @@
 var Preferences = require('./../config/Preferences.js'),
     User        = require('./../modules/user.js');
-    
+
 var _         = require('underscore'),
     cache     = require('memory-cache');
 
@@ -10,8 +10,9 @@ module.exports = {
             if(req.session.savedUrl) {
                 res.redirect(req.session.savedUrl);
             } else {
-                res.redirect(Preferences.path_on_server); //redirect to the base 
+                res.redirect(Preferences.path_on_server); //redirect to the base
             }
+            return;
         }
         var currentUser = _.findWhere(cache.get('storeStaff'), { 'username': User.getUser(req) });
         if(_.isUndefined(currentUser)) { //the user is not in the staff database
